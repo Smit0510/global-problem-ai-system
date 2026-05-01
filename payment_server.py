55947d85abe25cf6a5bbab65a89fb542dcfd47c3
Kyada Smit <smitkyada05@gmail.com>
2026-05-02 00:24:50 +0530

    Update payment_server.py

--- payment_server.py
+++ payment_server.py
@@ -30,4 +30,7 @@ def create_order():
 
 
 if __name__ == "__main__":
-    app.run()
+    if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
